a949e7efa3ebbb6f61baceed904ace643836e5cf
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-09-20 11:40:16 +0300

    clients controller created and routes for clients specified

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,33 @@
+class ClientsController < ApplicationController
+  def new
+    @client = Client.new
+  end
+
+  def index
+    @clients = Client.all
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      flash[:notice] = 'Client was successfully created.'
+
+      redirect_to clients_path
+    else
+      render :new
+    end
+  end
+
+  def update; end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:first_name, :last_name)
+  end
+end
