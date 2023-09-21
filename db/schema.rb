40be56f24c3d77f79db2dd8aef915855ede25f47
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-09-21 14:51:03 +0300

    expense model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_082637) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_114851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_082637) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "expenses", force: :cascade do |t|
+    t.decimal "amount"
+    t.text "description"
+    t.date "date"
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_expenses_on_client_id"
+  end
+
+  add_foreign_key "expenses", "clients"
 end
