21c75130496f5c97ecafcb4994ac2dc07246e0e1
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-09-21 14:53:37 +0300

    payment model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_114851) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_115235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,5 +31,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_114851) do
     t.index ["client_id"], name: "index_expenses_on_client_id"
   end
 
+  create_table "payments", force: :cascade do |t|
+    t.decimal "amount"
+    t.text "description"
+    t.date "date"
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_payments_on_client_id"
+  end
+
   add_foreign_key "expenses", "clients"
+  add_foreign_key "payments", "clients"
 end
