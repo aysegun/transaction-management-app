175b0905e2ecd71d241a5e24c850bc95683eb624
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-09-25 16:13:58 +0300

    changedmethod in the client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,22 +1,22 @@
+require 'bigdecimal'
+
 class Client < ApplicationRecord
   has_many :expenses
   has_many :payments
 
+  attribute :balance, :decimal, precision: 10, scale: 2, default: 0.0
+
   def credit(amount)
-    self.balance ||= 0  # Initialize balance to 0 if it's nil
-    self.balance += amount
+    self.balance += BigDecimal(amount.to_s)
     save
   end
 
   def debit(amount)
-    self.balance ||= 0  # Initialize balance to 0 if it's nil
-
-    # Ensure sufficient balance before debiting
-    if self.balance >= amount
-      self.balance -= amount
+    if self.balance >= BigDecimal(amount.to_s)
+      self.balance -= BigDecimal(amount.to_s)
       save
     else
-      errors.add(:balance, 'Insufficient balance')
+      errors.add(:balance, "Insufficient balance")
       false
     end
   end
