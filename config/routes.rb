9c4753cc80cb8d718c95525878215d90f80fd39a
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-10-02 12:21:51 +0300

    modified routes and paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+#   resources :clients do
+#     resources :transactions, only: %i[new show index] do
+#       post 'transactions/create_expense', to: 'transactions#create_expense', as: :create_expense
+#       post 'transactions/create_payment', to: 'transactions#create_payment', as: :create_payment
+#       delete 'transactions/:id', to: 'transactions#destroy', as: :destroy_transaction
+#     end
+#   end
+
+#   root 'clients#new'
+# end
+
+Rails.application.routes.draw do
   resources :clients do
-    resources :transactions
+    get 'transactions/new', to: 'transactions#new'
     post 'transactions/create_expense', to: 'transactions#create_expense', as: :create_expense
     post 'transactions/create_payment', to: 'transactions#create_payment', as: :create_payment
-    delete 'transactions/:id', to: 'transactions#destroy', as: :destroy_transaction
   end
 
   root 'clients#new'
